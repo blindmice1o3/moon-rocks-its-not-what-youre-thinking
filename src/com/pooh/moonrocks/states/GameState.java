da2424f98f570e5db278d88e87863657de05ba3b
jamesgov <blindmice1o3@gmail.com>
2019-02-08 16:01:42 -0800

    youtube's CodeNMore - NEW Beginner 2D Game Programming... finished video12. Took notes in a .txt file about the entity system (the class hierarchy of Entity [everything that isn't a Tile]).

--- src/com/pooh/moonrocks/states/GameState.java
+++ src/com/pooh/moonrocks/states/GameState.java
@@ -17,7 +17,7 @@ public class GameState extends State {
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.walkDown1, 0, 0, null);
+        g.drawImage(Assets.sailDown1, 0, 0, null);
     }
 
 } // **** end GameState class ****
