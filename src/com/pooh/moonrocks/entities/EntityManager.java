9f5f25e38de3edfd300d51c46873587e98af6f6f
jamesgov <blindmice1o3@gmail.com>
2019-02-13 11:46:14 -0800

    youtube's CodeNMore - finished video28: Render Order. Add an anonymous class implementation of the interface Comparator called renderSorter to the EntityManager class. This Comparator object sorts an ArrayList based on a customized compare(Object, Object) method, returning a negative number if the first argument is smaller (should come first) and a positive number if the first argument is bigger (should come after). This renderSorter is used in EntityManager's tick() method AFTER all of the Entity from the entities ArrayList gets updated (looped through all of their tick() method), it is passed into the entities ArrayList's sort() method to sort all Entity objects based on their BOTTOM y-coordinate. Then the render() method will be called after the tick() method, drawing the Entity objects in the world in the correct order (what should appear in front of or behind of other Entity objects).

--- src/com/pooh/moonrocks/entities/EntityManager.java
+++ src/com/pooh/moonrocks/entities/EntityManager.java
@@ -5,12 +5,28 @@ import com.pooh.moonrocks.entities.creatures.Player;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class EntityManager {
 
     private Handler handler;
     private Player player;
     private ArrayList<Entity> entities;
+    private Comparator<Entity> renderSorter = new Comparator<Entity>() {
+        // Negative number if a should be rendered BEFORE b (a is smaller, closer to 0, top of the screen).
+        // Positive number if a should be rendered AFTER b (a is larger, closer to bottom of screen).
+        @Override
+        public int compare(Entity a, Entity b) {
+            if (a.getY() + a.getHeight() < b.getY() + b.getHeight()) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+
+        // @@@ SHOULD be checking based on BOTTOM y-coordinate of bounding-box rather than the top-y. @@@
+        // That's why + a.getHeight().
+    };
 
     public EntityManager(Handler handler, Player player) {
         this.handler = handler;
@@ -24,8 +40,17 @@ public class EntityManager {
             Entity e = entities.get(i);             // Showing us ArrayList's get() method vs array's indexing [index].
             e.tick();                                   //FUTURE: collision stuff will probably depend on for-loop's index.
         }
+        // After updating the positions and etc for all the Entity objects in the game, SORT them based on y-coordinate
+        // to figure out what should be drawn on top of what (RENDERING ORDER).
+        entities.sort(renderSorter); // Pass in the customized anonymous Comparator class to sort based on y-coordinate.
     }
 
+    // RENDERING ORDER: CactusTree on top or Player on top? It depends on y-coordinate. If Player's y-coordinate is bigger
+    // than CactusTree's y-coordinate, then the Player's image should be drawn on top of (or after) the CactusTree's image.
+    // The larger the y-coordinate, the further down the screen it is. The closer to 0, the further up the screen.
+    // @@@ SHOULD be checking based on BOTTOM y-coordinate of bounding-box rather than the top-y. @@@
+    // @@@@@ NEED TO SORT BASED ON y-coordinate OF THE ENTITY. USE A Comparator! @@@@@
+    // Array of Entity will be sorted inside tick() before we render() the Entity.
     public void render(Graphics g) {                // Showing us for-each loop
         for (Entity e: entities) {                      //FUTURE: rendering collision stuff probably DON'T need index.
             e.render(g);
